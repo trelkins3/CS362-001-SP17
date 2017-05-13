4045affa0ae08b4caae9f64a2f37fa5cd44f972b
trelkins3 <elkinsd@flip1.engr.oregonstate.edu>
2017-05-13 16:42:50 -0700

    Added script, fixed POM

--- projects/elkinsd/assignment-3/Calendar/src/test/java/edu/osu/cs362/ApptTest.java
+++ projects/elkinsd/assignment-3/Calendar/src/test/java/edu/osu/cs362/ApptTest.java
@@ -8,37 +8,115 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ApptTest {
-    /**
-     * Test that the gets methods work as expected.
-     */
-	 @Test
-	  public void test01()  throws Throwable  {
-		 int startHour=13;
-		 int startMinute=30;
-		 int startDay=10;
-		 int startMonth=4;
-		 int startYear=2017;
-		 String title="Birthday Party";
-		 String description="This is my birthday party.";
-		 //Construct a new Appointment object with the initial data	 
-		 Appt appt = new Appt(startHour,
-		          startMinute ,
-		          startDay ,
-		          startMonth ,
-		          startYear ,
-		          title,
-		         description);
-	// assertions
-		 assertTrue(appt.getValid());
-		 assertEquals(13, appt.getStartHour());
-		 assertEquals(30, appt.getStartMinute());
-		 assertEquals(10, appt.getStartDay());
-		 assertEquals(04, appt.getStartMonth());
-		 assertEquals(2017, appt.getStartYear());
-		 assertEquals("Birthday Party", appt.getTitle());
-		 assertEquals("This is my birthday party.", appt.getDescription());         		
-	 }
-
-
 	
+	// Variables for all testing
+	int startHour=13;
+	int startMinute=30;
+	int startDay=10;
+	int startMonth=4;
+	int startYear=2017;
+	String title="Birthday Party";
+	String description="This is my birthday party.";
+	//Construct a new Appointment object with the initial data	 
+	Appt appt = new Appt(startHour,
+		        startMinute ,
+		        startDay ,
+		        startMonth ,
+		        startYear ,
+		        title,
+		       description);
+			   
+    
+    /* Test that the gets methods work as expected. */
+	@Test
+	public void test01()  throws Throwable  {
+		// assertions
+		assertTrue(appt.getValid());
+		assertEquals(13, appt.getStartHour());
+		assertEquals(30, appt.getStartMinute());
+		assertEquals(10, appt.getStartDay());
+		assertEquals(04, appt.getStartMonth());
+		assertEquals(2017, appt.getStartYear());
+		assertEquals("Birthday Party", appt.getTitle());
+		assertEquals("This is my birthday party.", appt.getDescription());         		
+	 }
+	 
+	 /* Test that the set methods work as expected */
+	@Test
+	public void test02()	throws Throwable	{
+		// assertions
+		assertTrue(appt.getValid());
+		appt.setStartHour(15);
+		assertTrue(appt.getValid());
+		appt.setStartMinute(29);
+		assertTrue(appt.getValid());
+		appt.setStartDay(9);
+		assertTrue(appt.getValid());
+		appt.setStartMonth(05);
+		assertTrue(appt.getValid());
+		appt.setStartYear(2016);
+		assertTrue(appt.getValid());
+		appt.setTitle(null);
+		assertEquals("", appt.getTitle());
+		appt.setTitle("Work");
+		assertEquals("Work", appt.getTitle());
+		appt.setDescription(null);
+		assertEquals("", appt.getDescription());
+		appt.setDescription("Gotta go to work!");
+		assertEquals("Gotta go to work!", appt.getDescription());
+	  }
+	  
+	  /* Test that isValid() catches bad input correctly */
+	@Test
+	public void test03()	throws Throwable	{
+		assertTrue(appt.getValid());
+		  
+		appt.setStartMonth(13);
+		assertFalse(appt.getValid());
+		appt.setStartMonth(0);
+		assertFalse(appt.getValid());
+		  
+		appt.setStartDay(32);
+		assertFalse(appt.getValid());
+		appt.setStartDay(0);
+		assertFalse(appt.getValid());
+		  
+		appt.setStartMinute(60);
+		assertFalse(appt.getValid());
+		appt.setStartMinute(-1);
+		assertFalse(appt.getValid());
+		  
+		appt.setStartHour(24);
+		assertFalse(appt.getValid());
+		appt.setStartHour(-1);
+		assertFalse(appt.getValid());
+		}
+		
+	/* Test that toString() outputs correctly */
+	@Test
+	public void test04()	throws Throwable	{
+		startHour=13;
+		startMinute=30;
+		startDay=10;
+		startMonth=4;
+		startYear=2017;
+		title="Birthday Party";
+		description="This is my birthday party.";
+		// Reset appointment object with the initial data	
+		appt = new Appt(startHour,
+			   startMinute ,
+		       startDay ,
+		       startMonth ,
+		       startYear ,
+		       title,
+		       description);
+				 
+		// assertions
+		assertTrue(appt.getValid());
+		assertEquals(appt.toString(), ("\t" + appt.getStartMonth()+"/"
+					+appt.getStartDay()+"/"+appt.getStartYear() + " at " 
+					+ 1 + ":" + appt.getStartMinute() + "pm" + " ," +
+					appt.getTitle() + ", " + appt.getDescription() + "\n"));
+		}
+		
 }
--- projects/elkinsd/assignment-3/Calendar/src/test/java/edu/osu/cs362/CalDayTest.java
+++ projects/elkinsd/assignment-3/Calendar/src/test/java/edu/osu/cs362/CalDayTest.java
@@ -11,11 +11,72 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+// Fun Fact: GregorianCalendar() default constructor generates off of local system time?
+
+// Does code outside of the test functions run between each test?
+
 public class CalDayTest {
-
-	 @Test
-	  public void test01()  throws Throwable  {
-
-	 }
+	GregorianCalendar cal = new GregorianCalendar(2017, 4, 30, 18, 30, 0);
+	CalDay testCal = new CalDay(cal);	// This fixed a bunch of null pointer errors...
 	
+	/* Test that ensures CalDay() constructors and set methods work. */
+	@Test
+	public void test01()	throws Throwable  {
+		testCal = new CalDay();
+		assertFalse(testCal.isValid());
+		
+		testCal = new CalDay(cal);
+		assertTrue(testCal.isValid()); 
+	}
+	
+	/* Test to check get methods. */
+	@Test
+	public void test02()	throws Throwable {
+		assertEquals(0, testCal.getSizeAppts());
+		assertEquals(30, testCal.getDay());
+		assertEquals(4, testCal.getMonth()); 
+		assertEquals(2017, testCal.getYear()); 
+		assertTrue(testCal.getAppts() != null);
+	}
+	
+	/* Test adding appointments to list. */
+	@Test
+	public void test03()	throws Throwable {
+		// Add valid appointment
+		testCal.addAppt(new Appt(19, 30, 30, 04, 2017, "Work", "Gotta go to work!"));
+		assertEquals(1, testCal.getSizeAppts()); 
+		
+		// Add second valid appointment
+		testCal.addAppt(new Appt(20, 30, 30, 04, 2017, "Meeting", "Have a meeting!"));
+		assertEquals(2, testCal.getSizeAppts()); 
+		
+		// Add bad appointment
+		testCal.addAppt(new Appt(0, 0, 0, 0, 0, null, null));
+		assertEquals(2, testCal.getSizeAppts()); 
+		
+		// Add appointment before other appointments
+		testCal.addAppt(new Appt(15, 30, 30, 04, 2017, "Work", "Gotta go to work!"));
+		assertEquals(3, testCal.getSizeAppts()); 
+	}
+	
+	/* Test if toString() functions correctly. */
+	@Test
+	public void test04()	throws Throwable {
+		testCal = new CalDay();
+		assertEquals("", testCal.toString());
+		
+		testCal = new CalDay(cal);
+		testCal.addAppt(new Appt(19, 30, 30, 04, 2017, "Work", "Gotta go to work!"));
+		testCal.addAppt(new Appt(15, 30, 30, 04, 2017, "Work", "Gotta go to work!"));
+		assertTrue(testCal.toString() != "");
+	}
+	
+	/* Test branching behavior of iterator(). */
+	@Test
+	public void test05()	throws Throwable {
+		assertTrue(testCal.iterator() != null);
+		
+		testCal = new CalDay();
+		assertTrue(testCal.iterator() == null);
+	}
 }
